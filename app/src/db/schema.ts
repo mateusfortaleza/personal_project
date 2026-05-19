235c6d6e66505db2527b1cdbe5af33f93012fed8
Mateus Helmer <mateusvillanova@protonmail.com>
2026-05-19 16:16:14 -0300

    Using Vercel Blob instead of locally sourced images.

--- app/src/db/schema.ts
+++ app/src/db/schema.ts
@@ -2,7 +2,7 @@ import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
 
 export const heroCardTable = pgTable("hero_card", {
     id: integer().primaryKey().generatedAlwaysAsIdentity(),
-    background_image: varchar({length: 200}).notNull(),
+    background_image: varchar({length: 1024}).notNull(),
     overlay_color: varchar({length: 7}).notNull(),
     title: varchar({length: 100}).notNull(),
     link: varchar({length: 255}).notNull()
